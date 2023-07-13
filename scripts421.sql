921e58723107603001b96a914dda56a9ff49f3d9
verzhik <verzhik11@gmail.com>
2023-07-13 18:19:08 +0300

    Database 2

---
+++ scripts421.sql
@@ -0,0 +1,9 @@
+ALTER TABLE student
+ADD CONSTRAINT age_constraint CHECK ( age > 16 ),
+ALTER COLUMN name SET NOT NULL,
+ADD CONSTRAINT name_unique UNIQUE (name),
+ALTER COLUMN age SET DEFAULT 20;
+
+ALTER TABLE faculty
+ADD PRIMARY KEY (name, color);
+

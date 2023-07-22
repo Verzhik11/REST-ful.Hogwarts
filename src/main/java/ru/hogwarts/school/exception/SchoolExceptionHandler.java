2bf6d33222c3eb20df166fc231e8018ba4dea0be
verzhik <verzhik11@gmail.com>
2023-07-22 12:38:36 +0300

    logger

--- src/main/java/ru/hogwarts/school/exception/SchoolExceptionHandler.java
+++ src/main/java/ru/hogwarts/school/exception/SchoolExceptionHandler.java
@@ -1,14 +1,18 @@
 package ru.hogwarts.school.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.hogwarts.school.service.StudentService;
 
 import javax.annotation.Resources;
 
 @RestControllerAdvice
 public class SchoolExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(SchoolExceptionHandler.class);
     @ExceptionHandler(
             {
                     FacultyNotFoundException.class,
@@ -17,11 +21,13 @@ public class SchoolExceptionHandler {
             }
     )
     public ResponseEntity<?> handleNotFound(RuntimeException e) {
+        logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(AvatarProcessingException.class)
     public ResponseEntity<?> handleInternalServerError() {
+        logger.error("Exception AvatarProcessingException was thrown");
         return ResponseEntity.internalServerError().build();
     }
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +29,7 @@ public class StudentService {
     private final FacultyRepository facultyRepository;
     private final FacultyMapper facultyMapper;
     private final AvatarService avatarService;
+    private final static Logger logger = LoggerFactory.getLogger(StudentService.class);
 
     public StudentService(StudentRepository studentRepository, StudentMapper studentMapper,
                           FacultyRepository facultyRepository, FacultyMapper facultyMapper, AvatarService avatarService) {
@@ -38,16 +41,19 @@ public class StudentService {
     }
 
     public StudentDtoOut createStudent (StudentDtoIn studentDtoIn) {
+        logger.info("Was invoked method for create student");
         return studentMapper.tDto(studentRepository.save(studentMapper.toEntity(studentDtoIn)));
     }
 
     public StudentDtoOut getStudentById(long id) {
+        logger.info("Was invoked method for get with id = {}", id);
         return studentRepository.findById(id)
                 .map(studentMapper::tDto)
                 .orElseThrow(() -> new StudentNotFoundException(id));
     }
 
     public StudentDtoOut updateStudent(long id, StudentDtoIn studentDtoIn) {
+        logger.info("Was invoked method for update with id = {}", id);
         return studentRepository.findById(id)
                 .map(oldStudent-> {
                     oldStudent.setAge(studentDtoIn.getAge());
@@ -61,6 +67,7 @@ public class StudentService {
     }
 
     public StudentDtoOut deleteStudent(long id) {
+        logger.info("Was invoked method for delete with id = {}", id);
         Student student = studentRepository.findById(id)
                 .orElseThrow(() -> new StudentNotFoundException(id));
         studentRepository.delete(student);
@@ -69,6 +76,7 @@ public class StudentService {
     }
 
     public List<StudentDtoOut> getStudentByAge(@Nullable Integer age) {
+        logger.info("Was invoked method for getStudentByAge");
         return Optional.ofNullable(age)
                 .map(studentRepository:: findByAge)
                 .orElseGet(studentRepository:: findAll).stream()
@@ -76,18 +84,21 @@ public class StudentService {
                 .collect(Collectors.toList());
     }
     public List<StudentDtoOut> findByAgeBetween(int min, int max) {
+        logger.info("Was invoked method for findByAgeBetween");
         return studentRepository.findByAgeBetween(min, max).stream()
                 .map(studentMapper ::tDto)
                 .collect(Collectors.toList());
     }
 
     public FacultyDtoOut getFacultyByStudent(long id) {
+        logger.info("Was invoked method for getFacultyByStudent");
         return studentRepository.findById(id)
                 .map(Student ::getFaculty)
                 .map(facultyMapper ::tDto)
                 .orElseThrow(() -> new StudentNotFoundException(id));
     }
     public StudentDtoOut uploadAvatar(long id, MultipartFile multipartFile) {
+        logger.info("Was invoked method for uploadAvatar");
         Student student = studentRepository.findById(id)
                 .orElseThrow(() -> new StudentNotFoundException(id));
         avatarService.create(student, multipartFile);
@@ -95,15 +106,18 @@ public class StudentService {
     }
 
     public Integer getCount() {
+        logger.info("Was invoked method for getCount");
        return studentRepository.getCount();
     }
 
     public Integer getAverageAge() {
+        logger.info("Was invoked method for getAverageAge");
         return studentRepository.getAverageAge();
     }
 
   @Transactional(readOnly = true)
     public List<StudentDtoOut> getLastFiveStudent() {
+      logger.info("Was invoked method for getLastFiveStudent");
         return studentRepository.getLastFiveStudent().stream()
                 .map(studentMapper :: tDto)
                 .collect(Collectors.toList());
